027dd760a53dc55708f8fc63f2d4ed851f587183
mikipejic <miki.pejic@yahoo.co.uk>
2024-07-24 14:22:42 +0100

    Added in logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HomePage from './HomePage';
 import PackSizesPage from './PackSizesPage';
+import logo from './assets/gymshark-logo.jpg'
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <nav>
+        <img src={logo} alt="Logo" className="logo" height={100} width={200}/>
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/calculate-pack-sizes">Order here</Link></li>
